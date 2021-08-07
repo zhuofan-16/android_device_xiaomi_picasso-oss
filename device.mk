37cacfc4cd33fc54ae783d55a43e9afc62f381ef
baolong24 <1667375308@qq.com>
2021-08-07 20:41:20 +0800

    picasso: Import XiaomiParts
     * From https://github.com/baolong24/android_device_xiaomi_picasso/tree/eleven/parts
    
    Signed-off-by: baolong24 <1667375308@qq.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ $(call inherit-product, vendor/xiaomi/picasso/picasso-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    NotchNoFillOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
@@ -162,6 +165,13 @@ PRODUCT_PACKAGES += \
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
 
 # Display
 PRODUCT_PACKAGES += \
