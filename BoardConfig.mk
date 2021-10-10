385d0a03433ed0746b055979a44575de9ac7b2d1
baolong24 <1667375308@qq.com>
2021-10-10 09:17:22 +0000

    [DNM]picasso: Set SELinux Premissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_DTB_OFFSET := 0x01F00000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -93,7 +94,7 @@ TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_CONFIG := gauguin_user_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/gauguin
+TARGET_KERNEL_SOURCE := kernel/xiaomi/picasso
 
 # Platform
 TARGET_BOARD_PLATFORM := lito
